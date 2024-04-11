dd7a6b258b8ea66255c8b40061a0d800a82e842b
Andrew <andy@andeh.uk>
2024-04-11 18:54:10 +0100

    Mute command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import io
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Union
 
 import discord
 from discord import app_commands
@@ -13,6 +13,52 @@ if TYPE_CHECKING:
 
 
 class Admin(Cog):
+    @app_commands.command(name="mute", description="Mute a member in the server")
+    @app_commands.default_permissions(moderate_members=True)
+    async def mute_member(
+        self,
+        itr: discord.Interaction[DevSocBot],
+        member: Union[discord.Member, discord.User],
+    ) -> None:
+        # We can safely assert here because we are using the guild_only decorator
+        assert itr.guild
+
+        if not isinstance(member, discord.Member):
+            return await itr.response.send_message(
+                f"**ERROR:** {member.mention} ({member}) is not in this server.",
+                ephemeral=True,
+            )
+
+        await itr.response.defer()
+
+        # TODO: Replace role IDs with actual values
+        servermute_role = itr.guild.get_role(123)
+        devsoc_role = itr.guild.get_role(123)
+        booster_role = itr.guild.get_role(123)
+
+        if not servermute_role:
+            return await itr.followup.send(
+                "**ERROR:** Server mute role not found", ephemeral=True
+            )
+        if not devsoc_role:
+            return await itr.followup.send(
+                "**ERROR:** DevSoc role not found", ephemeral=True
+            )
+        if not booster_role:
+            return await itr.followup.send(
+                "**ERROR:** Booster role not found", ephemeral=True
+            )
+
+        if servermute_role in member.roles:
+            await member.remove_roles(servermute_role)
+            await member.add_roles(devsoc_role)
+            await itr.followup.send(f"Unmuted {member.mention} ({member})!")
+        else:
+            roles_to_remove = [r for r in member.roles[1:] if r.id != booster_role.id]
+            await member.remove_roles(*roles_to_remove)
+            await member.add_roles(servermute_role)
+            await itr.followup.send(f"Muted {member.mention} ({member})!")
+
     @app_commands.command(
         name="unassigned",
         description="Checks for members that do not have a required role",
@@ -28,7 +74,7 @@ class Admin(Cog):
             announcement_role = itr.guild.get_role(668158580716732456)
             if not announcement_role:
                 await itr.response.send_message(
-                    "ERROR: `Announcement role not found`", ephemeral=True
+                    "**ERROR:** Announcement role not found", ephemeral=True
                 )
                 return
 
