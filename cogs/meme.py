8d38fba8b937a07fcc6d8ec0a0971c8ee8c55a0b
Andrew <andy@andeh.uk>
2024-04-20 14:50:04 +0100

    Meme cog

---
+++ cogs/meme.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from random import randrange
+from typing import TYPE_CHECKING
+
+import discord
+
+from data.memes import rat_fact
+from utils.cog import Cog
+from utils.types import GUILD_MESSAGEABLE
+
+if TYPE_CHECKING:
+    from bot import DevSocBot
+
+
+class Meme(Cog):
+    @Cog.listener("on_message")
+    async def rat_fact_listener(self, message: discord.Message) -> None:
+        if message.author.bot:
+            return
+
+        if not isinstance(message.channel, GUILD_MESSAGEABLE):
+            return
+
+        if message.embeds:
+            return
+
+        if "rat fact" in message.content.lower():
+            index = randrange(len(rat_fact))
+            await message.author.send(rat_fact[index])
+
+        elif "hello there" in message.content.lower():
+            await message.author.send(
+                "https://tenor.com/view/grevious-general-kenobi-star-wars-gif-11406339"
+            )
+
+        elif "beans" in message.content.lower():
+            embed = discord.Embed(color=0xE7EC11)
+            embed.set_image(url="https://i.imgur.com/GkyCNCH.jpg")
+            embed.set_footer(text="You thought i was gone? Shh")
+            await message.author.send(embed=embed)
+
+
+async def setup(bot: DevSocBot) -> None:
+    await bot.add_cog(Meme(bot))
